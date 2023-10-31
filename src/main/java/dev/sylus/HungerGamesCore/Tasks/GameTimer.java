9277077c550feb1c39702e4beb7b6b878a298cad
SylusSquared7 <sylussquared@proton.me>
2023-10-31 20:40:13 +0000

    Misc bugfixes and style changes

--- src/main/java/dev/sylus/HungerGamesCore/Tasks/GameTimer.java
+++ src/main/java/dev/sylus/HungerGamesCore/Tasks/GameTimer.java
@@ -107,16 +107,29 @@ public class GameTimer extends BukkitRunnable {
                 this.cancel();
                 this.stopGame(false);
                 return;
+
             } else if (deathmatchCountdown == 60 || deathmatchCountdown == 30 || deathmatchCountdown == 15 || deathmatchCountdown == 10 || deathmatchCountdown <= 5) {
                 Bukkit.broadcastMessage("§eA draw will be called in §c" + deathmatchCountdown + " §eseconds");
             } else if (deathmatchCountdown <= 114 && deathmatchCountdown >= 110) {
-                Bukkit.broadcastMessage("§ePlayers will become vulnerable in §c" + (114 - deathmatchCountdown + 1) + " §eseconds");
+
+                if (deathmatchCountdown == 114){ // I hate doing it like this but I could not think of any other way
+                    Bukkit.broadcastMessage("§ePlayers will become vulnerable in §c5 §eseconds");
+                } else if (deathmatchCountdown == 113){
+                    Bukkit.broadcastMessage("§ePlayers will become vulnerable in §c4 §eseconds");
+                } else if (deathmatchCountdown == 112){
+                    Bukkit.broadcastMessage("§ePlayers will become vulnerable in §c3 §eseconds");
+                } else if (deathmatchCountdown == 111){
+                    Bukkit.broadcastMessage("§ePlayers will become vulnerable in §c2 §eseconds");
+                } else if (deathmatchCountdown == 110){
+                    Bukkit.broadcastMessage("§ePlayers will become vulnerable in §c1 §eseconds");
+                }
+
             }
             if (deathmatchCountdown == 108){
                 for (Player players: Bukkit.getOnlinePlayers()){
                     players.playSound(players.getLocation(), Sound.ENTITY_ZOMBIE_HORSE_DEATH, 1, 1); // Replace this with the hypixel zombies one
                 }
-                Bukkit.broadcastMessage("§ePlayers are now vulnerable");
+                Bukkit.broadcastMessage("§a[!] Players are now vulnerable");
                 game.setVunrability(true);
             }
             deathmatchCountdown--;
