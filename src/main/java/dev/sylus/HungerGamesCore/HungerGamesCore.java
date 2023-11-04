b654b4b01ca2b0ba327cf78e2c940c24df1f41f1
SylusSquared7 <sylussquared@proton.me>
2023-11-04 01:28:43 +0000

    Removed a tempory command

--- src/main/java/dev/sylus/HungerGamesCore/HungerGamesCore.java
+++ src/main/java/dev/sylus/HungerGamesCore/HungerGamesCore.java
@@ -93,7 +93,6 @@ public final class HungerGamesCore extends JavaPlugin {
         getCommand("databaseTest").setExecutor(new DatabaseTest(databases)); // Remove this eventually
         getCommand("addPoints").setExecutor(new AddPoints(databases, scorebord));
         getCommand("resetPoints").setExecutor(new ResetPoints(databases, scorebord));
-        getCommand("getPlayerData").setExecutor(new GetAllData(databases, this));
 
         saveDefaultConfig();
 
